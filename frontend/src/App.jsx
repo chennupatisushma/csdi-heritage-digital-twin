8f711861c6ed3099df082739c7cd0f4ba071448f
chennupatisushma <sushmachennupati7981@gmail.com>
2025-12-29 12:18:27 +0530

    Deploy CSDI interactive demo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,8 @@
-import { useState } from "react";
-import CesiumMap from "./CesiumMap";
+import React from "react";
+import InnoportPoc from "./components/InnoportPoc";
 
-const API = import.meta.env.VITE_API_BASE_URL;
-
-export default function App() {
-  const [data, setData] = useState(null);
-
-  async function fetchFusion(lat, lon) {
-    const res = await fetch(
-      `${API}/api/fusion/predict?lat=${lat}&lon=${lon}`
-    );
-    setData(await res.json());
-  }
-
-  return (
-    <CesiumMap onMapClick={fetchFusion} data={data} />
-  );
+function App() {
+  return <InnoportPoc />;
 }
+
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "cesium/Build/Cesium/Widgets/widgets.css";
 
 window.CESIUM_BASE_URL = "/cesium";
 
